064f03502d5a531b259dee0f4cf5a52b376c6f57
Maja <majafarazin@gmail.com>
2021-11-07 00:02:42 +0100

    Napisala kodo, ki pobere vse knjige

---
+++ preberi-knjige.py
@@ -0,0 +1,18 @@
+import re
+
+vzorec_knjige = re.compile(
+    r'class="number">(?P<uvrstitev>\d+)</td>.*?'
+    r'<div id="(?P<id>\d+)".+?</div>.*?'
+    r'aria-level=\'4\'>(?P<naslov>.+?)</span>.*?'
+    r'<a class="authorName".+?itemprop="name">(?P<avtor>.+?)</span></a>.*?'
+    r'</span></span> (?P<povp_ocena>.+?) avg rating &mdash; (?P<st_ocen>.+?) ratings</span>.*?'
+    r'return false;">score: (?P<ocena>.+?)</a>.*?'
+    r'return false;">(?P<st_glasov>.+?) people voted</a><img',
+    flags=re.DOTALL
+)
+
+with open('best-books.html', 'r') as f:
+  vsebina = f.read()
+
+for i, knjiga in enumerate(vzorec_knjige.finditer(vsebina), 1):
+    print(i, knjiga.groupdict())
