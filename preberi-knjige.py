37f69bb2206a50763eb6271f3ab35743db78130b
Maja <majafarazin@gmail.com>
2021-11-08 10:48:25 +0100

    Popravila kodo

--- preberi-knjige.py
+++ preberi-knjige.py
@@ -5,13 +5,13 @@ vzorec_knjige = re.compile(
     r'<div id="(?P<id>\d+)".+?</div>.*?'
     r'aria-level=\'4\'>(?P<naslov>.+?)</span>.*?'
     r'<a class="authorName".+?itemprop="name">(?P<avtor>.+?)</span></a>.*?'
-    r'</span></span> (?P<povp_ocena>.+?) avg rating &mdash; (?P<st_ocen>.+?) ratings</span>.*?'
+    r'</span></span> (?P<povp_ocena>.+?) avg rating &mdash; (?P<st_ocen>.+?) ratings?</span>.*?'
     r'return false;">score: (?P<ocena>.+?)</a>.*?'
-    r'return false;">(?P<st_glasov>.+?) people voted</a><img',
+    r'return false;">(?P<st_glasov>.+?) (people|person) voted</a><img',
     flags=re.DOTALL
 )
 
-with open('best-books.html', 'r') as f:
+with open('best-books.html', 'r', encoding='utf-8') as f:
   vsebina = f.read()
 
 for i, knjiga in enumerate(vzorec_knjige.finditer(vsebina), 1):
