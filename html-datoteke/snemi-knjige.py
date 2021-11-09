6086eeb5b5703914af38a9677edb09c57d854fca
Maja <majafarazin@gmail.com>
2021-11-09 20:38:54 +0100

    Izbrisala nepotrebne datoteke

--- html-datoteke/snemi-knjige.py
+++
@@ -1,9 +0,0 @@
-import requests
-import os
-
-for page_num in range(91):
-    url = (f'https://www.goodreads.com/list/show/7.Best_Books_of_the_21st_Century?page={page_num + 1}')
-    response = requests.get(url)
-    vsebina = response.text
-    with open('best-books.html', 'a', encoding='utf-8') as dat:
-        dat.write(vsebina)
